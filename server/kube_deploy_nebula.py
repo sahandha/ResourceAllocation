a81976ac4a007a9a77a9bab83b7ae5622fb73071
Sahand <sahandha@gmail.com>
2018-08-20 16:39:43 -0500

    separate files for nebula

--- server/kube_deploy_nebula.py
+++ server/kube_deploy_nebula.py
@@ -145,7 +145,8 @@ def create_quota(namespace, maxmem="0Mi", maxcpu="0m", maxpods="0",priorityclass
                 kind="ResourceQuota",
                 metadata=client.V1ObjectMeta(name=namespace, namespace=namespace),
                 spec=client.V1ResourceQuotaSpec(
-                    hard={"cpu":maxcpu, "memory":maxmem, "pods":maxpods}
+                    hard={"cpu":maxcpu, "memory":maxmem, "pods":maxpods},
+                    scope_selector=client.V1ScopeSelector(match_expressions=[client.V1ScopedResourceSelectorRequirement(operator="In",name="PriorityClass",values=[priorityclass])])
                 )
             )
     pretty = 'true'
